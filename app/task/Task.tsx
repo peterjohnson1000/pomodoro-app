d0d5c267d404054555277af27a97eb013fc3a969
peterjohnson1000 <peterj1298@gmail.com>
2024-05-12 14:05:34 +0530

    added strikethorugh feature

--- app/task/Task.tsx
+++ app/task/Task.tsx
@@ -7,18 +7,35 @@ import { useState } from "react";
 const Alltask = () => {
 
     const [tasks, setTasks] = useState<string[]>([]);
+    const [isCompleted, setIsCompleted] = useState<Boolean[]>([]);
 
     const addTask = (e: any) => {
         e.preventDefault();
 
         if (e.target.taskUserInput.value) {
             setTasks([...tasks, e.target.taskUserInput.value]);
+            setIsCompleted([...isCompleted, false]);
             e.target.taskUserInput.value = "";
         }
     }
 
     const removeTask = (indexToRemove: number) => {
         setTasks(tasks.filter((_, index) => index !== indexToRemove));
+    }
+
+    const strikeCompletedTaks = (indexToStrike: number, isChecked: Boolean) => {
+        const test: Boolean[] = []
+
+        for (let index = 0; index < isCompleted.length; index++) {
+            if (index == indexToStrike) {
+                test.push(!isCompleted[index]);
+            }
+            else {
+                test.push(isCompleted[index]);
+            }
+        }
+
+        setIsCompleted(test)
     }
 
     return (
@@ -33,7 +50,8 @@ const Alltask = () => {
                     tasks.map(
                         (task, index) =>
                             <div key={task} className="flex mb-2">
-                                <h1 className="w-[250px] border-2 p-2 mr-2">{task}</h1>
+                                <input id="isCheckboxChecked" type="checkbox" onClick={() => strikeCompletedTaks(index, Boolean(!task[index]))} />
+                                <h1 className={`w-[250px] border-2 p-2 mr-2 ${isCompleted[index] ? "line-through" : "no-underline"}`}>{task}</h1>
                                 <button onClick={() => removeTask(index)} className="bg-red-600 text-white p-2 px-4 rounded-sm hover:bg-red-700">-</button>
                             </div>
                     )
