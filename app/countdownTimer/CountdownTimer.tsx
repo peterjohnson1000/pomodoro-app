19f8bfa8c9b50e8dba840c61944d2072684b1eb5
Peter Johnson <53271626+peterjohnson1000@users.noreply.github.com>
2024-05-30 15:13:40 +0530

    styled the timer display

--- app/countdownTimer/CountdownTimer.tsx
+++ app/countdownTimer/CountdownTimer.tsx
@@ -7,7 +7,6 @@ import { FaRegPauseCircle } from "react-icons/fa";
 import { MdOutlineNotStarted } from "react-icons/md";
 
 export default function CountdownTimer() {
-
     const hours: number = 0;
     const [minutes, setMinutes] = useState<number>(25);
     const [time, setTime] = useState<number>(0);
@@ -21,7 +20,6 @@ export default function CountdownTimer() {
         seconds:0
     });
     const startTimeRef = useRef<any>();
-
 
     const totalTimeCalculator = (hours: number, minutes: number) => {
         return ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000))
@@ -38,6 +36,7 @@ export default function CountdownTimer() {
                 //play a sound when timer naturally becomes 0
                 // playAudio();
                 setStop(false);
+                // notifyMe();
             }
         }
 
@@ -99,19 +98,57 @@ export default function CountdownTimer() {
         }
     }
 
+    const stopTimer = () => {
+        setTime(0); 
+        setPause(false); 
+        setStop(true);
+        setDisplayTimer({
+            hours:0,
+            minutes: 0,
+            seconds: 0
+        })
+    } 
+    
     const playAudio = () => {
         const audio = new Audio("https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3");
         audio.play();
     }
 
+    function notifyMe() {
+        if (Notification.permission === "granted") {
+          // Check whether notification permissions have already been granted;
+          // if so, create a notification
+          const notification = new Notification("Hi there!");
+          // …
+        } else if (Notification.permission !== "denied") {
+          // We need to ask the user for permission
+          Notification.requestPermission().then((permission) => {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+              const notification = new Notification("Hi there!");
+              // …
+            }
+          });
+        }
+    }
+
     return (
         <div className="flex justify-center flex-col items-center">
-            <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl">{displayTimer.hours}h : {displayTimer.minutes}m : {displayTimer.seconds}s</h1>
+            {/* <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl">{displayTimer.hours}h : {displayTimer.minutes}m : {displayTimer.seconds}s</h1> */}
+            
+            <div className="flex justify-center items-center">
+                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[120px]">{displayTimer.hours < 0 ? `0${displayTimer.hours}` : displayTimer.hours}h</h1>
+                <p className="mt-10 text-5xl">:</p>
+                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[130px]">{displayTimer.minutes < 0 ? `0${displayTimer.minutes}` : displayTimer.minutes}m</h1>
+                <p className="mt-10 text-5xl">:</p>
+                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[120px]">{displayTimer.seconds < 0 ? `0${displayTimer.seconds}` : displayTimer.seconds}s</h1>
+            </div>
+
             <div className="my-5">
                 {
                     time > 0 ?
                         <div className="flex justify-center items-center">
-                            <FaRegStopCircle className="text-4xl mr-3 hover:cursor-pointer" onClick={() => { setTime(0); setPause(false); setStop(true); }} />
+                            <FaRegStopCircle className="text-4xl mr-3 hover:cursor-pointer" onClick={stopTimer} />
                             {/* temp disabling the pause button */}
                             {/* <div onClick={() => setPause(!pause)}>
                                 {pause ? <MdOutlineNotStarted className="text-4xl hover:cursor-pointer" /> : <FaRegPauseCircle className="text-4xl hover:cursor-pointer" />}
@@ -127,8 +164,13 @@ export default function CountdownTimer() {
                                     <Input className="col-span-2 h-10 mr-2" type="text" value="loading" readOnly/> :
                                     <Input className="col-span-2 h-10 mr-2" type="number" value={minutes} onChange={(e) => minutesInputOnChange(e)} />    
                                 } */}
-
-                                <Input className="col-span-2 h-10 mr-2" type={initialDataFetched ? "number" : "text"} value={initialDataFetched == false ? "fetching minutes..." : minutes} onChange={(e) => minutesInputOnChange(e)} />
+                                {/* <Input className="col-span-2 h-10 mr-2" type={initialDataFetched ? "number" : "text"} value={initialDataFetched == false ? "fetching minutes..." : minutes} onChange={(e) => minutesInputOnChange(e)} /> */}
+                                {
+                                    !initialDataFetched ?
+                                    <p className="w-[185px] h-10 border p-2 rounded-md mr-2 my-1">fetching minutes...</p>
+                                    :
+                                    <Input className="w-[185px] h-10 mr-2 my-1" type="number" value={minutes ? minutes : ""} onChange={(e) => minutesInputOnChange(e)} />
+                                }
                                 <Button variant="outline" className="bg-green-600 text-white" onClick={setHoursAndMinutes}>Start Timer</Button>
                             </div>
                         </div>
