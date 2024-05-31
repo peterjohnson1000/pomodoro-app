8ef1093c750e54383cf56cb70cdc00acce6d217c
peterjohnson1000 <peterj1298@gmail.com>
2024-05-31 22:35:19 +0530

    undo task commit

--- app/task/Task.tsx
+++ app/task/Task.tsx
@@ -1,182 +1,94 @@
 'use client'
-import { useEffect, useRef, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
-import { FaRegStopCircle } from "react-icons/fa";
-import { FaRegPauseCircle } from "react-icons/fa";
-import { MdOutlineNotStarted } from "react-icons/md";
 
-export default function CountdownTimer() {
-    const hours: number = 0;
-    const [minutes, setMinutes] = useState<number>(25);
-    const [time, setTime] = useState<number>(0);
-    const [pause, setPause] = useState<boolean>(false); //temp disabling the pause button
-    const [stop, setStop] = useState<boolean>(false); //for playing audio(stop button pressed/not)
-    // const [dialogBoxOpen, setDialogBoxOpen] = useState<boolean>(false); //all related can be removed from the codebase
-    const [initialDataFetched, setInitialDataFetched] = useState<boolean>(false);
-    const [displayTimer, setDisplayTimer] = useState({
-        hours: 0,
-        minutes:0,
-        seconds:0
-    });
-    const startTimeRef = useRef<any>();
+//filter
+//clearInterval
+//custom hook in nextts
 
-    const totalTimeCalculator = (hours: number, minutes: number) => {
-        return ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000))
-    }
+const Alltask = () => {
+
+    const [tasks, setTasks] = useState<string[]>([]);
+    const [isCompleted, setIsCompleted] = useState<Boolean[]>([]);
+    const [initialDataFetched, setInitialDataFetched] = useState<boolean>(false); //fix for localStorage ref error
+    const [loaderState, setLoaderState] = useState<boolean>(false);
 
     useEffect(() => {
-        if (time > 0) {
-            document.title = formattedTime(time);;
+        if (initialDataFetched) {
+            localStorage.setItem("allTasks", JSON.stringify(tasks));
+            localStorage.setItem("completedTasks", JSON.stringify(isCompleted));
         }
-
-        if (time == 0) {
-            document.title = "0h : 0m : 0s"
-            if(!stop) {
-                //play a sound when timer naturally becomes 0
-                // playAudio();
-                setStop(false);
-                // notifyMe();
-            }
-        }
-
-        let timer: any;
-        if (!pause && time > 0) {
-            timer = setInterval(() => {
-                // due to the browser throttling issue we are going to use Date()
-                // even if the browser throttles we need to continue from elapsed time
-                let now = new Date().getTime(); //current time
-                const elapsedTime = now - (startTimeRef.current || now); //calculating the elapsed time
-                setTime((prevTime) => Math.max(prevTime - elapsedTime, 0));
-                startTimeRef.current = now;
-                // setTime((prevTime) => prevTime - 1000);
-            }, 1000)
-        }
-        else {
-            clearInterval(timer);
-        }
-
-        return () => clearInterval(timer);
-    }, [time, pause])
+    }, [tasks, isCompleted])
 
     useEffect(() => {
-        const minutesFromLocalStorage = localStorage.getItem("minutes");
-        if (!initialDataFetched && minutesFromLocalStorage) {
-            setMinutes(parseInt(minutesFromLocalStorage))
+        const allTasksFromLocalStoage = localStorage.getItem("allTasks");
+        const allCompletedTasksFromLocalStoage = localStorage.getItem("completedTasks");
+
+        if (!initialDataFetched && allTasksFromLocalStoage && allCompletedTasksFromLocalStoage) {
+            setTasks(JSON.parse(allTasksFromLocalStoage));
+            setIsCompleted(JSON.parse(allCompletedTasksFromLocalStoage));
         }
+
         setInitialDataFetched(true);
-    }, []);
+        setLoaderState(true);
+    }, [])
 
-    const formattedTime = (time: any) => {
-        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((time % (1000 * 60)) / 1000);
-
-        setDisplayTimer({
-                hours:hours,
-                minutes: minutes,
-                seconds: seconds
-            })
-
-        return `${hours}h : ${minutes}m : ${seconds}s`;
-    }
-
-    const setHoursAndMinutes = (e: any) => {
+    const addTask = (e: any) => {
         e.preventDefault();
 
-        // setDialogBoxOpen(!dialogBoxOpen)
-        const newTime = totalTimeCalculator(hours, minutes);
-        setTime(newTime)
-    }
+        if (e.target.taskUserInput.value) {
+            setTasks([...tasks, e.target.taskUserInput.value]);
+            setIsCompleted([...isCompleted, false]);
 
-    const minutesInputOnChange = (e: any) => {
-        if (e.target.value) {
-            setMinutes(parseInt(e.target.value));
-            localStorage.setItem("minutes", e.target.value);
-        } else {
-            setMinutes(0);
+            e.target.taskUserInput.value = "";
         }
     }
 
-    const stopTimer = () => {
-        setTime(0); 
-        setPause(false); 
-        setStop(true);
-        setDisplayTimer({
-            hours:0,
-            minutes: 0,
-            seconds: 0
-        })
-        startTimeRef.current = 0;
-    } 
-
-    const playAudio = () => {
-        const audio = new Audio("https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3");
-        audio.play();
+    const removeTask = (indexToRemove: number) => {
+        setTasks(tasks.filter((_, index) => index !== indexToRemove));
+        setIsCompleted(isCompleted.filter((_, index) => index !== indexToRemove));
     }
 
-    function notifyMe() {
-        if (Notification.permission === "granted") {
-          // Check whether notification permissions have already been granted;
-          // if so, create a notification
-          const notification = new Notification("Hi there!");
-          // …
-        } else if (Notification.permission !== "denied") {
-          // We need to ask the user for permission
-          Notification.requestPermission().then((permission) => {
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-              const notification = new Notification("Hi there!");
-              // …
-            }
-          });
-        }
+    const strikeCompletedTaks = (indexToStrike: number) => {
+        setIsCompleted(prevState =>
+            prevState.map((value, index) =>
+                index === indexToStrike ? !value : value
+            )
+        )
     }
 
     return (
-        <div className="flex justify-center flex-col items-center">
-            {/* <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl">{displayTimer.hours}h : {displayTimer.minutes}m : {displayTimer.seconds}s</h1> */}
-            
-            <div className="flex justify-center items-center">
-                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[120px]">{displayTimer.hours < 0 ? `0${displayTimer.hours}` : displayTimer.hours}h</h1>
-                <p className="mt-10 text-5xl">:</p>
-                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[130px]">{displayTimer.minutes < 0 ? `0${displayTimer.minutes}` : displayTimer.minutes}m</h1>
-                <p className="mt-10 text-5xl">:</p>
-                <h1 className="mt-10 border bg-black text-white p-5 rounded-xl text-5xl text-center w-[120px]">{displayTimer.seconds < 0 ? `0${displayTimer.seconds}` : displayTimer.seconds}s</h1>
-            </div>
+        <div className="flex flex-col justify-center items-center">
+            <form className="flex" onSubmit={addTask}>
+                <Input className="w-[250px] border-2 p-2 mr-2" placeholder="eat the frog first!" id="taskUserInput" autoComplete="off" />
+                <button type="submit" className="bg-red-600 text-white p-2 px-4 rounded-sm hover:bg-red-700">+</button>
+            </form>
 
-            <div className="my-5">
-                {
-                    time > 0 ?
-                        <div className="flex justify-center items-center">
-                            <FaRegStopCircle className="text-4xl mr-3 hover:cursor-pointer" onClick={stopTimer} />
-                            {/* temp disabling the pause button */}
-                            {/* <div onClick={() => setPause(!pause)}>
-                                {pause ? <MdOutlineNotStarted className="text-4xl hover:cursor-pointer" /> : <FaRegPauseCircle className="text-4xl hover:cursor-pointer" />}
-                            </div> */}
-                        </div>
-                        :
-                        <div>
-                            <p className="text-sm text-muted-foreground"> Enter time in minutes.</p>
-                            <div className="flex items-center">
-                                {/* value={minutes ? minutes : ""} */}
-                                {/* {
-                                    !initialDataFetched ?
-                                    <Input className="col-span-2 h-10 mr-2" type="text" value="loading" readOnly/> :
-                                    <Input className="col-span-2 h-10 mr-2" type="number" value={minutes} onChange={(e) => minutesInputOnChange(e)} />    
-                                } */}
-                                {/* <Input className="col-span-2 h-10 mr-2" type={initialDataFetched ? "number" : "text"} value={initialDataFetched == false ? "fetching minutes..." : minutes} onChange={(e) => minutesInputOnChange(e)} /> */}
-                                {
-                                    !initialDataFetched ?
-                                    <p className="w-[185px] h-10 border p-2 rounded-md mr-2 my-1">fetching minutes...</p>
-                                    :
-                                    <Input className="w-[185px] h-10 mr-2 my-1" type="number" value={minutes ? minutes : ""} onChange={(e) => minutesInputOnChange(e)} />
-                                }
-                                <Button variant="outline" className="bg-green-600 text-white" onClick={setHoursAndMinutes}>Start Timer</Button>
-                            </div>
-                        </div>
-                }
-            </div>
+            {
+                loaderState ?
+                    <div className="mt-5">
+                        {
+                            tasks.map(
+                                (task, index) =>
+                                    <div key={task + index} className="flex mb-2">
+                                        <input className="mr-2" id="isCheckboxChecked" type="checkbox" defaultChecked={Boolean(isCompleted[index])} onClick={() => strikeCompletedTaks(index)} />
+                                        <h1 className={`w-[235px] border-2 p-2 mr-2 ${isCompleted[index] ? "line-through" : "no-underline"}`}>{task}</h1>
+                                        <button onClick={() => removeTask(index)} className="bg-red-600 text-white p-2 px-4 rounded-sm hover:bg-red-700">-</button>
+                                    </div>
+                            )
+                        }
+                    </div>
+                    :
+                    <div role="status" className="pt-10">
+                        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
+                        <span className="sr-only text-black">Loading...</span>
+                    </div>
+            }
         </div>
-    );
-}
+    )
+};
+
+export default Alltask;
